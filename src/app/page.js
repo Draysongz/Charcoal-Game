46bb203e844f2d2271b488ddb73d75a7e54e07b6
Levi <70198955+Draysongz@users.noreply.github.com>
2024-12-09 00:11:45 +0100

    yeaaa

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import Landing from "./landingPage/page";
-import CreatureSuccess from "./creatureSuccess/page";
 
 export default function Home() {
   return (
     <div className="overflow-hidden overflow-x-hidden overflow-y-hidden">
 
-      {/* <Landing /> */}
-      <CreatureSuccess />
+      <Landing />
     </div>
   );
 }
